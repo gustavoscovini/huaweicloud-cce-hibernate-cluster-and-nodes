73bd04a5d486171ec523f9b8e49aba9567cd4497
Gustavo Vilela Mitraud <gvmitraud@protonmail.com>
2025-01-08 13:23:33 -0300

    fix: Fixed code where the server did not start after the cluster waked up

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
 from huaweicloudsdkcore.auth.credentials import BasicCredentials
 from huaweicloudsdkcce.v3.region.cce_region import CceRegion
-from huaweicloudsdkcce.v3 import CceClient, ListNodesRequest, HibernateClusterRequest
+from huaweicloudsdkcce.v3 import *
 from huaweicloudsdkecs.v2.region.ecs_region import EcsRegion
-from huaweicloudsdkecs.v2 import EcsClient, BatchStopServersRequest, BatchStopServersRequestBody, \
-    BatchStopServersOption, ServerId
+from huaweicloudsdkecs.v2 import *
 from huaweicloudsdkcore.exceptions import exceptions
 
 
@@ -55,20 +54,21 @@ def handler(event, context):
                 print(response_awake_C)
             except exceptions.ClientRequestException as e:
                 print_error("stopping cluster", e)
+            try:
+                print("trying to start server")
+                request = BatchStartServersRequest()
+                list_servers_to_start = [ServerId(id=uid) for uid in ids]
+                request.body = BatchStartServersRequestBody(os_start=BatchStartServersOption(servers=list_servers_to_start))
+
+                response = ecs_client.batch_start_servers(request)
+                print(response)
+            
+            except exceptions.ClientRequestException as e:
+                print_error("stopping servers". e)
 
     except exceptions.ClientRequestException as e:
         print("Could not stop servers")
-        print("trying to start servers")
-        try:
-            request = BatchStartServersRequest()
-            list_servers_to_start = [ServerId(id=uid) for uid in ids]
-            request.body = BatchStartServerRequestBody(os_start=BatchStartServersRequestOption(servers=list_servers_to_start))
-
-            response = ecs_client.batch_start_servers(request)
-            print(response)
-            
-        except exceptions.ClientRequestException as e:
-            print_error("stopping servers". e)
+        
         
 
 def print_error(action, e):
